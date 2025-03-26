eb5b0fdfe8ff5bc22548fa2e550c7f9e8fc4eb04
Jeslyn Lai <imlyj5@gmail.com>
2025-03-25 19:06:32 -0700

    fixed issues

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,4 +1,7 @@
 from random import randint
+HAND_SIZE = 10
+BONUS_MIN_LENGTH = 7
+LENGTH_BONUS_POINTS = 8
 
 LETTER_POOL = {
     'A': 9, 
@@ -58,74 +61,65 @@ SCORE_OF_LETTER = {
     'Z': 10
 }
 
+
 def draw_letters():
-    letter = []
-    letter_count = 0
-    key_list = list(LETTER_POOL.keys())
-    value_list = list(LETTER_POOL.values())
+    letters = []
+    available_pool = []
+    #To reflect the real possibility, we need to update the dictionary to list of letters
+    #In this way each letter in the pool will have same possibility to be selected by randint
+    for letter, counts in LETTER_POOL.items():
+        for count in range(counts):
+            available_pool. append(letter)
+    while len(letters) < HAND_SIZE:
+        random_index = randint(0, len(available_pool) - 1)
+        letters.append(available_pool.pop(random_index))  #update the pool
+    
+    return letters
 
-    while letter_count < 10:
-        random_index = randint(0, 25)
-
-        if value_list[random_index] > 0:
-            letter.append(key_list[random_index])
-            value_list[random_index] -= 1
-            letter_count += 1
-        else:
-            continue
-        
-        
-    return letter
-
- 
 
 
 def uses_available_letters(word, letter_bank):
     status = False
-    #Can we use .copy()?
-    #Can we use lower() or capitalize()?
     count_of_letter_mapping = {}
 
-    for i in letter_bank:
-        if i in count_of_letter_mapping:
-            count_of_letter_mapping[i]+= 1
+    for letter in letter_bank:
+        if letter in count_of_letter_mapping:
+            count_of_letter_mapping[letter]+= 1
         else:
-            count_of_letter_mapping[i]= 1
+            count_of_letter_mapping[letter]= 1
 
-    #print(count_of_letter_mapping)
-
-    for i in word:
-        if i.capitalize() in letter_bank and count_of_letter_mapping[i.capitalize()]>0:
-            count_of_letter_mapping[i.capitalize()] -= 1
+    for letter in word:
+        if letter.upper() in letter_bank and count_of_letter_mapping[letter.upper()]>0:
+            count_of_letter_mapping[letter.upper()] -= 1
             status = True
         else:
             status = False
+            return status  #add a return statement so that we return False immediately when seeing invalid letter
     return status
 
 
 def score_word(word):
     score = 0
-    for i in word:
-        score += SCORE_OF_LETTER[i.capitalize()]
-    if len(word) >= 7:
-        score += 8
+    for letter in word:
+        score += SCORE_OF_LETTER[letter.upper()]
+    if len(word) >= BONUS_MIN_LENGTH:
+        score += LENGTH_BONUS_POINTS
     return score
 
 def get_highest_word_score(word_list):
     highest_score = 0
     winner = ""
-    for i in word_list:
-        if score_word(i) > highest_score:
-            highest_score = score_word(i)
-            winner = i
-        elif score_word(i) == highest_score:
-            if len(i) < len(winner):
-                winner = i
-
-    for i in word_list:
-        if len(i) == 10:
-            winner = i
-            break
+    for word in word_list:
+        score_of_word = score_word(word)
+        if score_of_word > highest_score:
+            highest_score = score_of_word
+            winner = word
+        #Updated tie condition
+        elif score_of_word == highest_score:
+            if len(winner) != 10 and len(word) == 10:  #First check if the given word has 10 letters since it's preferable under tie condition
+                winner = word                              #Also check if the winner word already has 10 letters so that when multiple words having same length same score, we pick the first word.
+            elif len(winner) != 10 and len(word) < len(winner):  #If the given word doesn't have 10 letters, check if this word has fewer letters. 
+                winner = word                                        #Also need to check if the winner has 10 letters because we don't want to acdientally update the winner to word that has fewer letter (again, 10 letters is the most perferable under tie condition)
 
     return winner,highest_score
     
